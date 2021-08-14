42bbb04812dba85632cc4e5e92201781c23bec70
mariana280721 <mariana.nunez@gfe.frre.utn.edu.ar>
2021-08-14 15:40:19 -0300

    Commit de heroku fix Allowed_host

--- ProyectoInfo/settings.py
+++ ProyectoInfo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vt51imvxzio7bnpl$mug47pwegj-^f3p(d(9l8my)&nh)lseek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOST = ["https://proyectoinfo2021.herokuapp.com"]
+ALLOWED_HOST = ["*"]
 
 # Application definition
 
