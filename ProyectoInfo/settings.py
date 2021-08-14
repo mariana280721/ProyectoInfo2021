df89599dd21171f1922b5963a5404c401a4c756d
mariana280721 <mariana.nunez@gfe.frre.utn.edu.ar>
2021-08-14 15:55:50 -0300

    Allow debug

--- ProyectoInfo/settings.py
+++ ProyectoInfo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vt51imvxzio7bnpl$mug47pwegj-^f3p(d(9l8my)&nh)lseek'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['proyectoinfo2021.herokuapp.com', '127.0.0.1:8000']
 
